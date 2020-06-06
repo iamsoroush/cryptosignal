92e1e7ba997a51c3a3ddef12b1be7e78b9fab2a6
Soroush <soroush.moazed@gmail.com>
2020-06-06 16:13:39 +0430

    added new constants for agg_trade branch

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,14 @@
 import os
 import sys
+import pickle
 
-project_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-sys.path.append(project_dir)
+
+FIlE_DIR = os.path.dirname(os.path.realpath(__file__))
+ROOT_DIR = os.path.dirname(FIlE_DIR)
+sys.path.append(ROOT_DIR)
 
 # Paths
-DATA_DIR = os.path.join(project_dir, 'data')
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
 if not os.path.isdir(DATA_DIR):
     os.mkdir(DATA_DIR)
 TIME_DATA_DIR = os.path.join(DATA_DIR, 'time_data')
@@ -25,7 +28,7 @@ class TimeFrame:
 
 # General parameters
 BASE_CURRENCY_LIST = ('BTC', 'ETH', 'LINK', 'XRP', 'BNB', 'XTZ')
-TARGET_CURRENCY_LIST = ('USDT',)
+TARGET_CURRENCY_LIST = ('USDT', 'BTC')
 TIME_FRAMES = (TimeFrame('3m', 3),
                TimeFrame('15m', 15),
                TimeFrame('30m', 30),
@@ -37,8 +40,18 @@ TIME_FRAMES = (TimeFrame('3m', 3),
 TICK_BARS = (100, 500, 1000, 2000, 5000, 10000)
 TIME_DATA_MEMORY_IN_DAYS = 30 * 9
 
+
+# Aggregated Trades
+AGG_TRADE_COLLECTOR_MEMORY = 10
+
+with open(os.path.join(FIlE_DIR, 'n_trades.pkl'), 'rb') as f:
+    N_TRADES = pickle.load(f)
+N_DERIVATIVE_AGG_TRADE = 2
+GROUP_CHAT_ID = -1001119615266
+
+
 # Candle Processor
 PROFIT_INTERVALS = (4, 12)
-N_DERIVATIVES = 3
+N_DERIVATIVES = 2
 N_NEAREST_NEIGHBORS = 100
 COLLECTOR_MEMORY = 10  # How many candle do collector save to send to the saita's inference machine
